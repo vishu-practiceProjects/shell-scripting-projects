0dc6e8f915d3aa4fbb577816dec28d910d7e4d3f
vishu21 <32845994+vishu21@users.noreply.github.com>
2025-03-01 17:46:30 -0500

    Update list-users.sh
    helper function added, file information added

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+################
+# About:
+# Input:
+# Owner:
+################
+
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -34,6 +42,11 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+function helper{
+    expected_cmd_args=2
+    if[ $# -ne expected_cmd_args]; then
+    echo "please execute the script with required cmd args"
 }
 
 # Main script
